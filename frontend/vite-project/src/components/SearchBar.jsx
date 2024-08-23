7a55181df37210519432680bb6b6abc232956e99
Mihir Athale <athale.m@northeastern.com>
2024-08-22 22:37:21 -0400

    search bar and button styling

--- frontend/vite-project/src/components/SearchBar.jsx
+++ frontend/vite-project/src/components/SearchBar.jsx
@@ -1,14 +1,36 @@
 import React from "react";
 
 import {FaSearch} from 'react-icons/fa'
+import { useState } from 'react'
 
 import './SearchBar.css'
 
 export const SearchBar = () => {
+    const [query, setQuery] = useState('');
+
+    const fetchData = () => {
+        fetch('https://jsonplaceholder.typicode.com/users')}
+
+    const handleChange = (value) => {
+        setQuery(value)
+        fetchData(value)
+    }
+
+    const handleClick = () => {
+        handleChange(query)
+    }
     return (
+        <div className="app-container">
         <div className="input-wrapper">
             <FaSearch className="search-icon" />
-            <input type="text" placeholder="Enter your query" />
+            <input 
+            type="text" 
+            placeholder="Enter your query" 
+            value={query} 
+            onChange={(e) => setQuery(e.target.value)}/>
+            <div className="search-button-container" > <button className="search-button" onClick={handleClick}>Search</button></div>
+            <div className='search-results'></div>
+        </div>
         </div>
     );
 }
